702d1c8ae0b5bfdf012b83df4845439a03bc6e38
SaritGH <you@example.com>
2020-11-20 21:47:54 -0500

    cummon

--- src/main/java/Secondpipeline.java
+++ src/main/java/Secondpipeline.java
@@ -2,5 +2,6 @@ public class Secondpipeline {
     public static void main(String[] args) {
         System.out.println("This is my second pipeline ");
         System.out.println("Please make it work :(");
+        System.out.println("cummon");
     }
 }
